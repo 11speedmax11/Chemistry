1d21d5ae0e0019b6669c1db8dd02ade44e28eb6a
11speedmax11 <86107367+11speedmax11@users.noreply.github.com>
2023-06-14 13:04:19 +0300

    Update scriptLog.js

--- js/scriptLog.js
+++ js/scriptLog.js
@@ -1,9 +1,19 @@
-let button = document.querySelector(".enter");
+let button = document.querySelector(".enterUser");
 button.addEventListener("click", function () {
   let name = document.querySelector(".name").value;
-  let log = document.querySelector(".log").value;
-  if (name == "user")
-    window.location.href = "user.html";
-  if (name == "admin")
-    window.location.href = "admin.html";
+  let password = document.querySelector(".log").value;
+  let str = '../html/user.html'
+  // if (name != 'user' && password != 'user')
+  window.location.href = str;
+
+});
+
+let buttonAdm = document.querySelector(".enterAdmin");
+buttonAdm.addEventListener("click", function () {
+  let name = document.querySelector(".name").value;
+  let password = document.querySelector(".log").value;
+  // if (name != 'admin' && password != 'admin')
+
+  window.location.href = '../html/admin.html';
+
 });
